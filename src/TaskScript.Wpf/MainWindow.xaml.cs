940e9d3dabeae649164a181be60e720fe9f308d5
Amadeusz Wieczorek <git@amadeusw.com>
2016-05-20 01:38:57 -0700

    nicely runs scripts with params

--- src/TaskScript.Wpf/MainWindow.xaml.cs
+++ src/TaskScript.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +21,66 @@ namespace TaskScript.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ScriptRunner _runner;
+        private string _params;
+
+        public String Output
+        {
+            set
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    if (!String.IsNullOrWhiteSpace(value)
+                        && value.Trim() != ">") {
+                        Result.Text = value;
+                    }
+                });
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            _runner = new ScriptRunner(this);
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
-
+            _runner.RunScript(@"C:\Users\amade\Documents\test.csx", _params ?? Params.Text);
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
-
+            _runner.RunScript(@"C:\Users\amade\Documents\test2.csx", _params ?? Params.Text);
         }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            _runner.RunScript(@"C:\Users\amade\Documents\power.csx", _params ?? Params.Text);
+        }
+
+        private void Params_Click(object sender, RoutedEventArgs e)
+        {
+            Params.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _runner.Stop();
+        }
+
+        private void Params_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _params = Params.Text;
+                Params.Visibility = Visibility.Collapsed;
+            }
+            if (e.Key == Key.Escape)
+            {
+                Params.Visibility = Visibility.Collapsed;
+            }
+        }
+
     }
 }
